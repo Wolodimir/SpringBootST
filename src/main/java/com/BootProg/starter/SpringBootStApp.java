b56745002a5b2df2be5ec23c73687ad96ae8e8ec
Wolodimir <dvdnent666@gmail.com>
2020-11-17 20:42:31 +0400

    Structure

---
+++ src/main/java/com/BootProg/starter/SpringBootStApp.java
@@ -0,0 +1,12 @@
+package com.BootProg.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringBootStApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStApp.class, args);
+    }
+}
